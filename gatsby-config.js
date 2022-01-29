dbc198415c72a9473b02f612fa8ef2062920d4c0
kemalgencay <kemalgencay@gmail.com>
2022-01-29 07:03:33 +0300

    ilk edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `tutorial`,
+    title: `Öğrenmek yaparak olur`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
---
+++ src/pages/blog/index.js
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../../components/layout"
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      ))}
+    </Layout>
+  )
+}
+export const query = graphql`
+  query MyQuery {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "D MMMM YYYY", locale: "tr")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`
+export default BlogPage
