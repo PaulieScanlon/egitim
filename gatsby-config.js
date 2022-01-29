02232bd7235459b3fccfdf529048b089589b1c75
kemalgencay <kemalgencay@gmail.com>
2022-01-29 06:33:20 +0300

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: `tutorial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
+}
