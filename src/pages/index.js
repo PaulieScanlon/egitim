28a89d0eab53404be057a952c7b425a712ac7910
kemalgencay <kemalgencay@gmail.com>
2022-01-30 07:44:19 +0300

    tekrar image

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,8 @@ const IndexPage = () => {
         <p> *bilgisayar değil, bilgi soyuttur sayılamaz!</p>
       </p>
       <StaticImage
-        alt="Zuzu ve Kemal"
-        src="/home/kemal/egitim/src/images/ZuzuKemal.jpg"
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
       />
     </Layout>
   )
