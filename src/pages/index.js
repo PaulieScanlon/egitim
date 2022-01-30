008774436fa10728fd7e5909fca83905861265b6
kemalgencay <kemalgencay@gmail.com>
2022-01-30 08:32:34 +0300

    eaüt

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,6 @@ const IndexPage = () => {
         <p> *bilgisayar değil, bilgi soyuttur sayılamaz!</p>
       </p>
       <StaticImage
-      <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
       />
