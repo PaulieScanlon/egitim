a1f837cbd1d3a5372f55dfc4ec43664e2bd45677
kemalgencay <kemalgencay@gmail.com>
2022-01-30 08:23:03 +0300

    Zuzu

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,8 @@ const IndexPage = () => {
         <p> *bilgisayar değil, bilgi soyuttur sayılamaz!</p>
       </p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="Zuzu ve Kemal"
+        src="~/egitim/src/images/ZuzuKemal.jpg"
       />
     </Layout>
   )
